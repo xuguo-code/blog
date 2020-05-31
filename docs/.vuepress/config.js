e919682d7d179ab047e667e2570bfc0455438ab4
xuguo <xuguo@leimingtech.com>
2020-05-31 10:34:28 +0800

    fix(vue3): 修复路由错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,8 +18,8 @@ module.exports = {
         title: 'Vue3',
         collapsable: true,
         children: [
-          ['vue3/', 'vue3现状'],
-          ['vue3/reactivity', 'vue3的响应式']
+          ['Vue3/', 'vue3现状'],
+          ['Vue3/reactivity', 'vue3的响应式']
         ]
       },
       {
